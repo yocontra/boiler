1a9e76a1c3a8b91e7a47f3a50c78421990a3e3e1
Contra <contra@maricopa.edu>
2012-10-30 14:57:25 -0700

    push

--- app/js/People.js
+++ app/js/People.js
@@ -1,7 +1,14 @@
 define(function(){
   var People = dermis.collection({
     getAvailable: function(){
+      var out = [];
+      this.get('items').forEach(function(item){
+        if (!item.get('busy')) {
+          out.push(item);
+        }
+      });
 
+      return out;
     }
   });
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,16 +6,22 @@ define(["js/Person", "js/People"], function(Person, People){
 
     var john = Person.create();
     john.set('name', 'John');
-    john.set('currentProject', 'Nothing');
+    john.set('photo', 'http://cdn.nesn.com/cat/cat_john_beattie_48.jpg');
+    john.set('busy', true);
+    john.set('currentProject', 'Hacking');
     employees.push(john);
 
     var paul = Person.create();
     paul.set('name', 'Paul');
-    paul.set('currentProject', 'Nothing');
+    paul.set('busy', false);
+    paul.set('currentProject', null);
     employees.push(paul);
     
 
     employees.bind($("#main"));
+
+    window.employees = employees;
+    window.Person = Person;
   };
 
   return app;
